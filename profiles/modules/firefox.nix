63e69df6320a479d152ca97a4652841df3986ba8
zxfstd <me@gnux.cn>
2022-04-21 07:18:40 +0800

    firefox 0.0.0

--- profiles/modules/firefox.nix
+++ profiles/modules/firefox.nix
@@ -1,57 +1,9 @@
-# { pkgs, lib, config, options, ... }:
-
-# let
-#   cfg = config.my.modules.firefox;
-# in
-
-# {
-#   options = with lib; {
-#     my.modules.firefox = {
-#       enable = mkEnableOption ''
-#         Whether to enable firefox module
-#       '';
-#     };
-#   };
-
-#   config = with lib;
-#     mkIf cfg.enable {
-#       programs.firefox = {
-#         enable = true;
-#         package = if pkgs.stdenv.isDarwin then
-#           pkgs.runCommand "firefox-0.0.0" { } "mkdir $out"
-#         else
-#           pkgs.firefox-bin;
-#         # extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-#         #   ublock-origin
-#         #   onepassword-password-manager
-#         # ];
-#         # profiles =
-#         #   let defaultSettings = {
-#         #     "app.update.auto" = false;
-#         #     "browser.startup.homepage" = "about:blank";
-#         #     # *snip* no need to splurge all my settings, you get the idea...
-#         #     # "identity.fxaccounts.account.device.name" = config.networking.hostName;
-#         #     "signon.rememberSignons" = false;
-#         #   };
-#         #   in
-#         #   {
-#         #     default = {
-#         #       isDefault = true;
-#         #       settings = {
-#         #         "signon.rememberSignons" = false;
-#         #       };
-#         #     };
-#         #   };
-#       };
-#     };
-# }
 { pkgs, lib, config, options, ... }:
 
 let
   cfg = config.my.modules.firefox;
 
 in
-
 {
   options = with lib; {
     my.modules.firefox = {
@@ -63,11 +15,17 @@ in
 
   config = with lib;
     mkIf cfg.enable {
-      # my = {
-      #   user.packages = with pkgs; [
-      #     firefox-mac
-      #   ];
-      # };
+      my = {
+        user.packages = with pkgs; [
+          firefox-mac
+        ];
+        # Wire up Tridactyl native for macOS.
+        hm.file."Library/Application Support/Mozilla/NativeMessagingHosts" = {
+        source =
+          "${pkgs.tridactyl-native}/lib/mozilla/native-messaging-hosts";
+          recursive = true;
+        };
+      };
       environment = {
         systemPackages = [
           pkgs.firefox-mac
