d9dd04e8a43b6ad4ca875b37a23814f097e0d5e1
zxfstd <me@gnux.cn>
2022-04-30 07:06:33 +0800

    emacs flake

--- flake.nix
+++ flake.nix
@@ -41,14 +41,14 @@
             "https://cache.nixos.org"
             "https://nix-community.cachix.org"
             "https://nixpkgs.cachix.org"
-            "https://zxfstd.cachix.org"
+            #"https://zxfstd.cachix.org"
           ];
 
           binaryCachePublicKeys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
-            "zxfstd.cachix.org-1:3Q1gyqgA9NsOshOgknDvc6fhA8gw0PFAf2qs5vJpeLU="
+            #"zxfstd.cachix.org-1:3Q1gyqgA9NsOshOgknDvc6fhA8gw0PFAf2qs5vJpeLU="
           ];
 
           gc = {
