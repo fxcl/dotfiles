7d0d6aaf33509225f519280a2aa12fb3c26d9e4c
fxcl <me@gnux.cn>
2022-09-05 20:42:42 +0800

    no message

--- profiles/darwin/programs/zsh.nix
+++ profiles/darwin/programs/zsh.nix
@@ -99,6 +99,7 @@
     XDG_BIN_HOME = "$HOME/.local/bin";
     #XDG_RUNTIME_DIR="/tmp/$(id -u)-runtime-dir";
     #XDG_RUNTIME_DIR="$HOME/.run";
+    XDG_RUNTIME_DIR = "$HOME/.local/run";
 
     NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/config";
     NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
@@ -133,6 +134,30 @@
     export HOMEBREW_INSTALL_BADGE="🍵"
     export RUST_SRC_PATH=$(rustc --print sysroot)/lib/rustlib/src/rust/library
   '';
+
+  initExtra = ''
+      extract () {
+        if [ -f $1 ] ; then
+          case $1 in
+            *.tar.bz2)   tar xvjf $1    ;;
+            *.tar.gz)    tar xvzf $1    ;;
+            *.bz2)       bunzip2 $1     ;;
+            *.rar)       unrar x $1       ;;
+            *.gz)        gunzip $1      ;;
+            *.tar)       tar xvf $1     ;;
+            *.tbz2)      tar xvjf $1    ;;
+            *.tgz)       tar xvzf $1    ;;
+            *.zip)       unzip $1       ;;
+            *.Z)         uncompress $1  ;;
+            *.7z)        7z x $1        ;;
+            *)           echo "don't know how to extract '$1'..." ;;
+          esac
+        else
+          echo "'$1' is not a valid file!"
+        fi
+      }
+    '';
+
   oh-my-zsh = {
     enable = true;
 
