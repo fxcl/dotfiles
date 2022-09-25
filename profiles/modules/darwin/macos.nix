07ac7512393f1f826c8ee34a30be6503b0b637f7
fxcl <me@gnux.cn>
2022-09-25 20:12:13 +0800

    del latg

--- profiles/modules/darwin/macos.nix
+++ profiles/modules/darwin/macos.nix
@@ -301,7 +301,6 @@ in
       # Photos - Prevent Photos from opening automatically when devices are plugged in
       defaults write com.apple.ImageCapture disableHotPlug -bool true
     '';
-  };
 
       };
 
