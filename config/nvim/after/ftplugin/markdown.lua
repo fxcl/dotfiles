76041dfee396af1da6d9fa08f39530544631850b
fxcl <me@gnux.cn>
2022-10-14 18:18:38 +0800

    nvm

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -1,11 +1,13 @@
 local utils = require '_.utils'
 
+vim.opt.conceallevel = 2
+
 utils.plaintext()
 
 if vim.fn.executable 'gh' == 1 then
-  vim.cmd [[nnoremap <buffer><leader>p :call utils#openMarkdownPreview()<CR>]]
+	vim.cmd [[nnoremap <buffer><leader>p :call utils#openMarkdownPreview()<CR>]]
 end
 
 if vim.fn.executable 'glow' == 1 then
-  vim.cmd [[nnoremap <buffer><leader>g :Glow<CR>]]
+	vim.cmd [[nnoremap <buffer><leader>g :Glow<CR>]]
 end
