d574c327d584a0e1bb1b6d13849d77fb646317bd
okk <me@gnux.cn>
2023-09-06 21:43:06 +0800

    1

---
+++ machines/addsa.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, lib, inputs, ... }: {
+
+  nix = {
+    gc = { user = config.my.username; };
+    # Auto upgrade nix package and the daemon service.
+    # services.nix-daemon.enable = true;
+    # nix.package = pkgs.nix;
+    # nix.maxJobs = 4;
+    # nix.buildCores = 4;
+  };
+
+  my = {
+    modules = {
+      macos.enable = true;
+      gpg.enable = true;
+    };
+  };
+
+  homebrew.casks = [
+    # "arq" # I need a specific version so I will handle it myself.
+    # "transmit"
+    # "jdownloader"
+    # "signal"
+  ];
+
+  networking = {
+    hostName = "addsa";
+    computerName = "addsa";
+  };
+
+  system.stateVersion = 4;
+}
